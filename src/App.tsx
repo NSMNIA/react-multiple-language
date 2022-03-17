60d45a24c86f59b2a57e4d2cb5aad0315c691a9d
Kevin Stoop <433243@student.fontys.nl>
2022-03-17 15:13:01 +0100

    👌 translation working
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ const App = (props: Props) => {
   return (
     <div>
       <h1>{t("welcome_to_react")}</h1>
+      <p>
+        {t("paragraph")}
+      </p>
       <SelectLanguage />
     </div>
   )
