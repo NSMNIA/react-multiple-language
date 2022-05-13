f740272630a49970218caf64d5b97da9fad56ab6
Kevin Stoop <433243@student.fontys.nl>
2022-05-13 11:23:27 +0200

    Updates

--- locales/en/translation.js
+++
@@ -1,4 +0,0 @@
-{
-    "welcome_to_react": "Welcome to React",
-    "paragraph": "Hello, this is a multiple language test."
-}
--- locales/es/translation.js
+++
@@ -1,4 +0,0 @@
-{
-    "welcome_to_react": "Bienvenido a React",
-    "paragraph": "Hola, esta es una prueba de varios idiomas."
-}
